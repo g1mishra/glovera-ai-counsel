f4bbe783e9ac946cd08c88569b2ddc61938f63f1
Jeevan Kumar <g1mishra.dev@gmail.com>
2024-11-23 00:44:41 +0530

    minor fix

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
+import GoogleIcon from '@/components/icons/google';
 import { signIn } from 'next-auth/react';
-import Image from 'next/image';
 
 export default function SignIn() {
   return (
@@ -17,12 +17,7 @@ export default function SignIn() {
             onClick={() => signIn('google', { callbackUrl: '/' })}
             className="w-full flex items-center justify-center gap-3 px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm text-sm font-medium text-gray-700 dark:text-gray-200 bg-white dark:bg-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            <Image
-              src="/google.svg"
-              alt="Google Logo"
-              width={20}
-              height={20}
-            />
+            <GoogleIcon className="w-6 h-6" />
             Continue with Google
           </button>
         </div>
